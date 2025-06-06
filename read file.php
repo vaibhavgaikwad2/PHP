e67c8ece888f7019731d289ea06613d3f9cccacf
vaibhavgaikwad2 <vaibhavgaikwad7007@gmail.com>
2025-06-06 18:42:38 +0530

    update

--- read file.php
+++ read file.php
@@ -1,6 +1,6 @@
 <?php
 
-$file = "files/hello.txt";
+$file = "C:/xampp\htdocs/tutorial\PHP/files/hello.txt";
 $myfile = fopen($file,'r') or die("unable to open the file");
 
 echo fread($myfile,filesize($file));
