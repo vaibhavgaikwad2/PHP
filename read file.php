2e24403e323c4b63296cbc52ed1cda0f88dec386
vaibhavgaikwad2 <vaibhavgaikwad7007@gmail.com>
2025-06-06 19:09:11 +0530

    Update read file.php

--- read file.php
+++ read file.php
@@ -1,12 +1,26 @@
 <?php
+// this is basic method 
+// $file = "C:/xampp\htdocs/tutorial\PHP/files/hello.txt";
+// $myfile = fopen($file,'r') or die("unable to open the file");
 
-$file = "C:/xampp\htdocs/tutorial\PHP/files/hello.txt";
-$myfile = fopen($file,'r') or die("unable to open the file");
+// echo fread($myfile,filesize($file));
 
-echo fread($myfile,filesize($file));
+// fclose($myfile);
 
-fclose($myfile);
+if(isset($_FILES['file'])){
+    $file=$_FILES['files']['tmp_name'];
+    $myfile=fopen($file,"r");
 
-
+    echo fread($myfile,filesize($file));
+    fclose($myfile);
+}
 
 ?>
+
+<form action ='' method="post" enctype = "multipart/form-data">
+    <input type="file" name="file" />
+    <br />
+    <br />
+    <button> read the file </button>
+</form>
+
