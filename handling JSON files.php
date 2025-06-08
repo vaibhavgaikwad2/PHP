653367404ea27a6634c1cefb436c88dd3342eec3
vaibhavgaikwad2 <vaibhavgaikwad7007@gmail.com>
2025-06-08 19:27:41 +0530

    update

--- handling JSON files.php
+++ handling JSON files.php
@@ -1,5 +1,9 @@
 <?php
 
-//
+/*
+JSON stands for javascript object notation.
+it is text based data exchange format for structuring data. 
+it is mainly used to exchange data in between a server and web application
 
+*?
 ?>
