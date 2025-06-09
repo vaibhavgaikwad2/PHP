ec1573e4dcc40b81f219088b97f34fc3d29730a8
vaibhavgaikwad2 <vaibhavgaikwad7007@gmail.com>
2025-06-09 19:16:30 +0530

    update

--- handling JSON files.php
+++ handling JSON files.php
@@ -4,6 +4,14 @@
 JSON stands for javascript object notation.
 it is text based data exchange format for structuring data. 
 it is mainly used to exchange data in between a server and web application
+All datatypes are allowed 
+it is inherited from objects of js thats why its name is JSON
 
-*?
+to convert dtype into json use json_encode
+*/
+
+
+
+
+
 ?>
