75d086e44bce839d9028f9e29405eb7bd1ca23d3
Vaibhav Gaikwad <vaibhavgaikwad7007@gmail.com>
2025-06-17 20:23:49 +0530

    update

--- Database in php/Read data from mysql.php
+++ Database in php/Read data from mysql.php
@@ -5,10 +5,27 @@ $getStudents = $conn->prepare("SELECT * FROM student"); // prepare is used to ex
 
 $getStudents->execute(); // this used to execute the above command
 
-$student = $getStudents->fetchAll(); // This used to fetch all data from db 
+$students = $getStudents->fetchAll(); // This used to fetch all data from db 
 
-echo "<pre>";
+// echo "<pre>";
 
-print_r($student);
+// print_r($students);
+
+echo "<table border ='1'>";
+echo "<tr>
+        <th>Name</th>
+        <th>Marks</th>
+        <th>Grade</th>
+        <th>City</th>
+      </tr>";
+foreach($students as $student){
+    echo "<tr>
+        <td>". $student['name']. "</td>
+        <td>". $student['marks']. "</td>
+        <td>". $student['grade']. "</td>
+        <td>". $student['city']. "</td>
+        </tr>";
+}
+echo "<table>";
 
 ?>
