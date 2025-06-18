17854ba645603d80bf581ed54ef277bdfbc131dc
Vaibhav Gaikwad <vaibhavgaikwad7007@gmail.com>
2025-06-18 12:35:42 +0530

    update

---
+++ Database in php/dynamic data insertion.php
@@ -0,0 +1,28 @@
+
+<form action="" method="post">
+<input type ="text" placeholder ="enter name" name="name">
+<br/>
+<br/>
+<input type ="number" placeholder ="enter your marks" name="marks">
+<br/>
+<br/>
+<input type ="text" placeholder ="enter your grade" name="grade">
+<br/>
+<br/>
+<input type ="text" placeholder ="enter city name" name="city">
+<br/>
+<br/>
+<input type ="number" placeholder ="enter age" name="age">
+<br/>
+<br/>
+<button>submit</button>
+
+
+</form>
+
+
+<?php
+
+
+
+?>
