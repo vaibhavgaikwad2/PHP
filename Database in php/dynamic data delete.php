ed6c0c18e43ff29c5441ce9c03723fb715d542ac
Vaibhav Gaikwad <vaibhavgaikwad7007@gmail.com>
2025-06-18 18:14:17 +0530

    update

---
+++ Database in php/dynamic data delete.php
@@ -0,0 +1,31 @@
+<?php
+
+include("./config.php");
+
+$query = $conn->prepare("SELECT * FROM student");
+$query ->execute();
+$students=$query->fetchAll();
+
+echo "<table border='1' style='margin:auto' >";
+echo "<tr>
+        <th>Roll no</th>
+        <th>Name</th>
+        <th>Marks</th>
+        <th>Grade</th>
+        <th>City</th>
+        <th>Delete</th>
+    </tr>";
+foreach($students as $student){
+echo "<tr>
+        <td>".$student['rollno']."</td>
+        <td>".$student['name']."</td>
+        <td>".$student['marks']."</td>
+        <td>".$student['grade']."</td>
+        <td>".$student['city']."</td>
+        <td><form method='post'>
+        <button name=delete value=".$student['rollno'].">Delete</button>
+        </form></td>
+    </tr>";
+}
+echo "</table>";
+?>
