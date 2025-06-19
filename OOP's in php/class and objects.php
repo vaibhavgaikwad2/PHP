e5f2214acfc9162658461a8d5d6dc3572e03b049
Vaibhav Gaikwad <vaibhavgaikwad7007@gmail.com>
2025-06-19 22:02:47 +0530

    update

--- OOP's in php/class and objects.php
+++ OOP's in php/class and objects.php
@@ -1,5 +1,22 @@
 <?php
 
+class Math{
+
+     /* best practises for class 
+    1) one file one class
+    2) file name = class name
+    3)
+    */
+
+    function sum(){
+        echo 10+20;
+    }
+
+}
+// objects
+$obj1= new Math();           // this is object 1 
+
+$obj1->sum();   // calling function 
 
 
 ?>
