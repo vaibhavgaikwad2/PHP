9209f6c7522480808fa3a5a439ac3aa690fb082e
Vaibhav Gaikwad <vaibhavgaikwad7007@gmail.com>
2025-06-21 00:45:42 +0530

    update

---
+++ OOP's in php/typehinting.php
@@ -0,0 +1,21 @@
+<?php
+
+/*
+Type hinting is a feature in PHP that allows you to specify the expected 
+data type (e.g., int, string, array, class name) for function parameters,
+ return values, and class properties to ensure type safety.
+
+
+Why Use It?
+
+    Prevents bugs by enforcing correct data types.
+
+    Improves code readability and auto-suggestions in IDEs.
+
+    Helps in debugging and maintenance.
+
+    reduces servers time by handling error before code execution
+*/
+
+
+?>
