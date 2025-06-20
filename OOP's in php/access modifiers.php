534390ce9332beeb5688d5a1ded7fde6c82e589f
Vaibhav Gaikwad <vaibhavgaikwad7007@gmail.com>
2025-06-20 19:02:29 +0530

    update

--- OOP's in php/access modifiers.php
+++ OOP's in php/access modifiers.php
@@ -3,9 +3,9 @@
 /* it decides where we can access the methods and properties.
 
 Access Zone -->
-Access inside the cureent class only -->protected
+Access inside the cureent class only -->private
 Access ecerywhere -->public
-Access in inherited class --> private --> outside the class cannot be accessed
+Access in inherited class --> protected -->
 
 Types -->
 private
@@ -16,12 +16,24 @@ class Teacher{
     private function questionPapers(){
         echo "question paper for students";
     }
-    function exam(){
+    function exam(){ //  by default this method is public
         $this->questionPapers();
+    }
+
+    protected function marks(){
+        echo "all students marks";
+    }
+}
+class Management extends Teacher{
+    function reviewMarks(){
+        $this->marks();
     }
 }
 
 $obj1= new Teacher();
 //$obj1->questionPapers(); // here direct access is prohibated
 $obj1->exam();
+echo "<br/>";
+$obj=new Management();
+$obj->reviewMarks();
 ?>
