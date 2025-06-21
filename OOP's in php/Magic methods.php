bb759664bb554bd58dac80f83a42ff25df06b7a8
Vaibhav Gaikwad <vaibhavgaikwad7007@gmail.com>
2025-06-21 15:55:09 +0530

    update

--- OOP's in php/Magic methods.php
+++ OOP's in php/Magic methods.php
@@ -1,5 +1,36 @@
 <?php
 
+// it is an special methods that are automatically invoked by certain actions
 
+/*
+1) __construct()
+2) __destruct() --> when the script is completed
+                --> when the object of class is destroyed
+3)__invoke() --> object ko as function call kar sakte hai
+4)__get() --> it gives warning for undefined property
+*/
+class User{
+    public $name="vaibhav";
+    function getName(){
+        echo "my name is vaibhav";
+    }
+
+    function __invoke(){
+        echo "I am vaibhav";
+    }
+    
+    function __get($property){
+        echo "$property property is not exists";
+    }
+
+
+}
+
+$user = new User();
+
+$user();
+echo "<br/>";
+
+echo $user->$nama;
 
 ?>
