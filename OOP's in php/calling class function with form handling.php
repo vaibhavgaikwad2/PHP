41c9bb060fa149fe47ca5f921df90a3ce2808a62
Vaibhav Gaikwad <vaibhavgaikwad7007@gmail.com>
2025-06-21 14:35:04 +0530

    update

--- OOP's in php/calling class function with form handling.php
+++ OOP's in php/calling class function with form handling.php
@@ -1,23 +1,21 @@
-<?php
-
-if(isset($_POST['user'])){
-    class User{
-        function getName(){
-            echo "User name is $_POST['user']";
-        }
-    }
-
-    $user=new User();
-    $user->getName();
-}
-
-?>
-
 <form action ="" method="post">
     <input type="text" name="user" placeholder="enter username">
     <br/>
     <br/>
     <button>click</button>
-
-
 </form>
+
+<?php
+
+if(isset($_POST['user'])){
+    class User{
+        function getName($name){
+            echo "User name is $name";
+        }
+    }
+
+    $user=new User();
+    $user->getName($_POST['user']);
+}
+
+?>
